a05bc713f3f4ed2ce0ffa338feb786de0b49b6e5
Aleksandr Behma <alexbegma@gmail.com>
2024-05-07 11:47:17 +0300

    layout main page first version

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,28 @@
-console.log('Daddy')
+// window.addEventListener('scroll', function() {
+// document.getElementById('header-nav').classList.toggle('headernav-scroll', window.scrollY > 115);
+// });
+
+$(document).ready(function(){
+  $(".owl-carousel-full").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        500: {
+          items: 2
+      },
+        600: {
+            items: 3
+        },
+        900: {
+          items: 4
+      },
+        1200: {
+            items: 5
+        }
+      }
+  });
+});
